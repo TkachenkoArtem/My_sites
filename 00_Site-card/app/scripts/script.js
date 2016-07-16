cd72010d7cc54727ae941e73f2e82efce995365d
TkachenkoArtem <igrfy87@gmail.com>
2016-07-16 23:12:12 +0300

    init siteCard

---
+++ 00_Site-card/app/scripts/script.js
@@ -0,0 +1,43 @@
+/*BACK-TO-TOP*/
+
+(function() {
+    if ($('#back-to-top').length) {
+        var scrollTrigger = 100, // px
+            backToTop = function() {
+                var scrollTop = $(window).scrollTop();
+                if (scrollTop > scrollTrigger) {
+                    $('#back-to-top').addClass('show');
+                } else {
+                    $('#back-to-top').removeClass('show');
+                }
+            };
+
+        backToTop();
+        $(window).on('scroll', function() {
+            backToTop();
+        });
+        $('#back-to-top').on('click', function(e) {
+            e.preventDefault();
+            $('html,body').animate({
+                scrollTop: 0
+            }, 700);
+        });
+    }
+})();
+
+/*Onload*/
+(function() {
+    document.addEventListener("DOMContentLoaded", function() {
+        document.getElementsByTagName('main')[0].innerHTML = '<p class="elegantshadow elegantshadow--main">Junior<br>Web-developer</p>';
+    });
+})();
+
+/*loadHome*/
+function loadHome() {
+    document.getElementsByTagName('main')[0].innerHTML = '<p class="elegantshadow elegantshadow--main">Junior<br>Web-developer</p>';
+}
+
+/*loadHome*/
+function loadAboutme() {
+    document.getElementsByTagName('main')[0].innerHTML = 'ffhdghfg';
+}
