83c0808c5970d6a95d3fbfea0b0e2f020db83b3c
TkachenkoArtem <igrfy87@gmail.com>
2016-09-02 03:17:12 +0300

    new skills

--- 00_Site-card/app/scripts/script.js
+++ 00_Site-card/app/scripts/script.js
@@ -3,26 +3,34 @@
 /*BACK-TO-TOP*/
 
 (function() {
-    if ($('#back-to-top').length) {
-        var scrollTrigger = 100, // px
-            backToTop = function() {
-                var scrollTop = $(window).scrollTop();
-                if (scrollTop > scrollTrigger) {
-                    $('#back-to-top').addClass('show');
-                } else {
-                    $('#back-to-top').removeClass('show');
-                }
-            };
+  if ($('#back-to-top').length) {
+    var scrollTrigger = 100, // px
+      backToTop = function() {
+        var scrollTop = $(window).scrollTop();
+        if (scrollTop > scrollTrigger) {
+          $('#back-to-top').addClass('show');
+        } else {
+          $('#back-to-top').removeClass('show');
+        }
+      };
 
-        backToTop();
-        $(window).on('scroll', function() {
-            backToTop();
-        });
-        $('#back-to-top').on('click', function(e) {
-            e.preventDefault();
-            $('html,body').animate({
-                scrollTop: 0
-            }, 700);
-        });
-    }
+    backToTop();
+    $(window).on('scroll', function() {
+      backToTop();
+    });
+    $('#back-to-top').on('click', function(e) {
+      e.preventDefault();
+      $('html,body').animate({
+        scrollTop: 0
+      }, 700);
+    });
+  }
 })();
+
+/*TOOLTIP*/
+$(document).ready(function() {
+  $("body").tooltip({
+    selector: "[data-toggle='tooltip']",
+    container: "body"
+  });
+});
