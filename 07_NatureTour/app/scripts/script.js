1a7c9740d4663f1dcc85192251aad273fe2576dc
TkachenkoArtem <igrfy87@gmail.com>
2016-09-15 13:34:40 +0300

    project completed

--- 07_NatureTour/app/scripts/script.js
+++ 07_NatureTour/app/scripts/script.js
@@ -38,83 +38,94 @@ tabs();
 Reference: http://jsfiddle.net/BB3JK/47/
 */
 
-$('select').each(function(){
-    var $this = $(this), numberOfOptions = $(this).children('option').length;
+$('select').each(function() {
+  var $this = $(this),
+    numberOfOptions = $(this).children('option').length;
 
-    $this.addClass('select__hidden');
-    $this.wrap('<div class="select"></div>');
-    $this.after('<div class="select__styled"></div>');
+  $this.addClass('select__hidden');
+  $this.wrap('<div class="select"></div>');
+  $this.after('<div class="select__styled"></div>');
 
-    var $styledSelect = $this.next('div.select__styled');
-    $styledSelect.text($this.children('option').eq(0).text());
+  var $styledSelect = $this.next('div.select__styled');
+  $styledSelect.text($this.children('option').eq(0).text());
 
-    var $list = $('<ul />', {
-        'class': 'select__options'
-    }).insertAfter($styledSelect);
+  var $list = $('<ul />', {
+    'class': 'select__options'
+  }).insertAfter($styledSelect);
 
-    for (var i = 0; i < numberOfOptions; i++) {
-        $('<li />', {
-            text: $this.children('option').eq(i).text(),
-            rel: $this.children('option').eq(i).val()
-        }).appendTo($list);
-    }
+  for (var i = 0; i < numberOfOptions; i++) {
+    $('<li />', {
+      text: $this.children('option').eq(i).text(),
+      rel: $this.children('option').eq(i).val()
+    }).appendTo($list);
+  }
 
-    var $listItems = $list.children('li');
+  var $listItems = $list.children('li');
 
-    $styledSelect.click(function(e) {
-        e.stopPropagation();
-        $('div.select__styled--active').not(this).each(function(){
-            $(this).removeClass('select__styled--active').next('ul.select__options').hide();
-        });
-        $(this).toggleClass('select__styled--active').next('ul.select__options').toggle();
+  $styledSelect.click(function(e) {
+    e.stopPropagation();
+    $('div.select__styled--active').not(this).each(function() {
+      $(this).removeClass('select__styled--active').next('ul.select__options').hide();
     });
+    $(this).toggleClass('select__styled--active').next('ul.select__options').toggle();
+  });
 
-    $listItems.click(function(e) {
-        e.stopPropagation();
-        $styledSelect.text($(this).text()).removeClass('select__styled--active');
-        $this.val($(this).attr('rel'));
-        $list.hide();
-        //console.log($this.val());
-    });
+  $listItems.click(function(e) {
+    e.stopPropagation();
+    $styledSelect.text($(this).text()).removeClass('select__styled--active');
+    $this.val($(this).attr('rel'));
+    $list.hide();
+    //console.log($this.val());
+  });
 
-    $(document).click(function() {
-        $styledSelect.removeClass('select__styled--active');
-        $list.hide();
-    });
+  $(document).click(function() {
+    $styledSelect.removeClass('select__styled--active');
+    $list.hide();
+  });
 
 });
 
 /*truncateText*/
 
 (function($) {
-    var truncate = function(el) {
-        var text = el.text(),
-            height = el.height(),
-            clone = el.clone();
+  var truncate = function(el) {
+    var text = el.text(),
+      height = el.height(),
+      clone = el.clone();
 
-	  clone.css({
-            position: 'absolute',
-            visibility: 'hidden',
-            height: 'auto'
-        });
-        el.after(clone);
+    clone.css({
+      position: 'absolute',
+      visibility: 'hidden',
+      height: 'auto'
+    });
+    el.after(clone);
 
-        var l = text.length - 1;
-        for (; l >= 0 && clone.height() > height; --l) {
-            clone.text(text.substring(0, l) + '...');
-        }
+    var l = text.length - 1;
+    for (; l >= 0 && clone.height() > height; --l) {
+      clone.text(text.substring(0, l) + '...');
+    }
 
-        el.text(clone.text());
-        clone.remove();
-    };
+    el.text(clone.text());
+    clone.remove();
+  };
 
-    $.fn.truncateText = function() {
-        return this.each(function () {
-            truncate($(this));
-        });
-    };
+  $.fn.truncateText = function() {
+    return this.each(function() {
+      truncate($(this));
+    });
+  };
 }(jQuery));
 
 $(function() {
-    $('.article__text').truncateText();
+  $('.article__text').truncateText();
+});
+
+/*SLIDER*/
+
+$('.slick').slick({
+  autoplay: true,
+  autoplaySpeed: 2000,
+  arrows: false,
+  // infinite: false,
+  // edgeFriction: 0.3
 });
