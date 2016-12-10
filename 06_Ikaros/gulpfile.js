ce59c2d54ca93b3092da1252a30c717b850e1146
TkachenkoArtem <igrfy87@gmail.com>
2016-12-10 03:57:52 +0200

    fixed package

--- 06_Ikaros/gulpfile.js
+++ 06_Ikaros/gulpfile.js
@@ -1,110 +1,20 @@
-"use strict";
+/*jshint esversion: 6 */
 
-var gulp = require('gulp');
-// var concatCSS = require('gulp-concat-css');
-var cleanCSS = require('gulp-clean-css');
-var rename = require("gulp-rename");
-var pngmin = require('gulp-pngmin');
-var htmlmin = require('gulp-htmlmin');
-var base64 = require('gulp-base64-inline');
-var sass = require('gulp-sass');
-var autoprefixer = require('gulp-autoprefixer');
-// var uncss = require('gulp-uncss');
-var uglify = require('gulp-uglify');
-var livereload = require('gulp-livereload');
-var haml = require('gulp-haml');
-var spritesmith = require('gulp.spritesmith');
-
-/*минимизация html*/
-gulp.task('html', () => {
-    return gulp.src('app/*.html')
-        .pipe(htmlmin({
-            collapseWhitespace: true
-        }))
-        .pipe(rename('index.min.html'))
-        .pipe(gulp.dest('dist'))
-        .pipe(livereload());
-});
-
-/*создание haml*/
-gulp.task('haml', function () {
-  gulp.src('app/*.haml')
-    .pipe(haml())
-    .pipe(htmlmin({
-        collapseWhitespace: true
-    }))
-    .pipe(rename('index.min.html'))
-        .pipe(gulp.dest('dist'))
-        .pipe(livereload());
-});
-
-/*объединение сторонних css*/
-// gulp.task('vendorCSS', () => {
-//     return gulp.src('app/css/*.vendor.css')
-//         .pipe(concatCSS('vendor.css'))
-//         .pipe(uncss({
-//             html: ['app/index.html']
-//         }))
-//         .pipe(cleanCSS({
-//             debug: true
-//         }, function(details) {
-//             console.log(details.name + ': ' + details.stats.originalSize);
-//             console.log(details.name + ': ' + details.stats.minifiedSize);
-//         }))
-//         .pipe(rename('vendor.min.css'))
-//         .pipe(gulp.dest('dist/css/'));
-// });
-
-/*минимизация png*/
-gulp.task('png', () => {
-    return gulp.src('app/img/*.png')
-        .pipe(pngmin())
-        .pipe(gulp.dest('dist/img'));
-});
-
-/*создание css*/
-gulp.task('sass', () => {
-    return gulp.src('app/scss/**/*.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('app/css'));
-
-});
-
-/*операции с css*/
-gulp.task('css', () => {
-    return gulp.src('app/css/style.css')
-        // .pipe(uncss({
-        //     html: ['app/index.html']
-        // }))
-        .pipe(autoprefixer({
-            browsers: ['last 100 versions'],
-            cascade: false
-        }))
-        .pipe(base64('../img'))
-        .pipe(cleanCSS({
-            debug: true
-        }, function(details) {
-            console.log(details.name + ': ' + details.stats.originalSize);
-            console.log(details.name + ': ' + details.stats.minifiedSize);
-        }))
-        .pipe(rename('style.min.css'))
-        .pipe(gulp.dest('dist/css/'))
-        .pipe(livereload());
-});
-
-/*операции с js*/
-gulp.task('compress', () => {
-    return gulp.src('app/scripts/*.js')
-        .pipe(uglify())
-        .pipe(rename('script.min.js'))
-        .pipe(gulp.dest('dist/scripts/'));
-});
-
-/*операции со шрифтами*/
-gulp.task('copyfonts', function() {
-    gulp.src('app/fonts/*.{ttf,woff,woff2,eot,otf,svg,icon}')
-        .pipe(gulp.dest('dist/fonts'));
-});
+const gulp = require('gulp');
+const concatCSS = require('gulp-concat-css');
+const cleanCSS = require('gulp-clean-css');
+const rename = require("gulp-rename");
+const imagemin = require('gulp-imagemin');
+const htmlmin = require('gulp-htmlmin');
+const base64 = require('gulp-base64-inline');
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const uncss = require('gulp-uncss');
+const uglify = require('gulp-uglify');
+const livereload = require('gulp-livereload');
+const haml = require('gulp-haml');
+const babel = require('gulp-babel');
+const spritesmith = require('gulp.spritesmith');
 
 /*создание спрайта*/
 gulp.task('sprite', function() {
@@ -125,33 +35,107 @@ gulp.task('sprite', function() {
     spriteData.css.pipe(gulp.dest('app/scss')); // путь, куда сохраняем стили
 });
 
+/*создание haml*/
+gulp.task('haml', () => {
+  gulp.src('app/index.haml')
+    .pipe(haml())
+    .pipe(htmlmin({
+      collapseWhitespace: true
+    }))
+    .pipe(rename('index.html'))
+    .pipe(gulp.dest('dist'))
+    .pipe(livereload());
+});
+
+/*объединение сторонних css*/
+gulp.task('vendorCSS', () => {
+  return gulp.src('app/css/*.vendor.css')
+    .pipe(concatCSS('vendor.css'))
+    .pipe(uncss({
+      html: ['dist/index.html']
+    }))
+    .pipe(cleanCSS({
+      debug: true
+    }, (details) => {
+      console.log(`${details.name} : ${details.stats.originalSize}`);
+      console.log(`${details.name} : ${details.stats.minifiedSize}`);
+    }))
+    .pipe(rename('vendor.min.css'))
+    .pipe(gulp.dest('dist/css/'));
+});
+
+/*создание css*/
+gulp.task('sass', () => {
+  return gulp.src('app/scss/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('app/css'));
+});
+
+/*операции с css*/
+gulp.task('css', () => {
+  return gulp.src('app/css/style.css')
+    .pipe(autoprefixer({
+      browsers: ['last 100 versions'],
+      cascade: false
+    }))
+    .pipe(base64('../img'))
+    .pipe(cleanCSS({
+      debug: true
+    }, (details) => {
+      console.log(details.name + ': ' + details.stats.originalSize);
+      console.log(details.name + ': ' + details.stats.minifiedSize);
+    }))
+    .pipe(rename('style.min.css'))
+    .pipe(gulp.dest('dist/css/'))
+    .pipe(livereload());
+});
+
+/*транспайлинг*/
+gulp.task('babel', () => {
+  return gulp.src('app/scripts/script.js')
+    .pipe(babel())
+    .pipe(rename('script_after_babel.js'))
+    .pipe(gulp.dest('app/scripts'));
+});
+
+/*операции с js*/
+gulp.task('compress', () => {
+  return gulp.src('app/scripts/script_after_babel.js')
+    .pipe(uglify())
+    .pipe(rename('script.min.js'))
+    .pipe(gulp.dest('dist/scripts/'))
+    .pipe(livereload());
+});
+
+/*минимизация image*/
+gulp.task('imagemin', () => {
+  return gulp.src('app/img/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('dist/img'));
+});
+
 /*инициализация*/
 gulp.task('init', () => {
-    // gulp.src('node_modules/font-awesome/css/font-awesome.css')
-    //     .pipe(rename('font-awesome.vendor.css'))
-    //     .pipe(gulp.dest('app/css/'));
-    // gulp.src('node_modules/normalize-css/normalize.css')
-    //     .pipe(rename('normalize.vendor.css'))
-    //     .pipe(gulp.dest('app/css/'));
-    // gulp.src('node_modules/font-awesome/fonts/*.{ttf,woff,woff2,eot,otf,svg}')
-    //     .pipe(gulp.dest('app/fonts'));
-    gulp.src('app/img/*.ico')
-        .pipe(gulp.dest('dist/img'));
+  gulp.src('node_modules/normalize.css/normalize.css')
+    .pipe(rename('normalize.vendor.css'))
+    .pipe(gulp.dest('app/css/'));
+  gulp.src('app/img/*.ico')
+    .pipe(gulp.dest('dist/img'));
 });
 
 /*WATCH*/
 gulp.task('watch', () => {
-    livereload.listen();
-    gulp.watch('app/*.html', ['html', 'vendorCSS']) /*следить за html, запускать html и vendorCSS*/
-    gulp.watch('app/*.haml', ['haml']) /*следить за haml, запускать haml*/
-    // gulp.watch('app/css/*.min.css', ['vendorCSS']) /*следить за сторонними css, запускать vendorCSS*/
-    gulp.watch('app/img/*.png', ['png']) /*следить за изображениями, запускать png*/
-    gulp.watch('app/scss/**/*.scss', ['sass']) /*следить за SASS, запускать sass*/
-    gulp.watch('app/css/style.css', ['css']) /*следить за style.css, запускать css*/
-    gulp.watch('app/scripts/*.js', ['compress']) /*следить за js, запускать compress*/
+  livereload.listen();
+  gulp.watch('app/*.haml', ['haml']); /*следить за haml, запускать haml*/
+  gulp.watch('app/img/for_dist/*', ['imagemin']); /*следить за изображениями, запускать imagemin*/
+  gulp.watch('app/css/*.min.css', ['vendorCSS']); /*следить за сторонними css, запускать vendorCSS*/
+  gulp.watch('app/scss/**/*.scss', ['sass']); /*следить за SASS, запускать sass*/
+  gulp.watch('app/css/style.css', ['css']); /*следить за style.css, запускать css*/
+  gulp.watch('app/scripts/script.js', ['babel']); /*следить за js, запускать babel*/
+  gulp.watch('app/scripts/script_after_babel.js', ['compress']); /*следить за js, запускать compress*/
 });
 
 /*BUILD*/
-gulp.task('build', ['init', 'haml', 'html', 'png', 'sass', 'css', 'compress', 'copyfonts'], function() {
-    console.log('Building completed!');
-})
+gulp.task('build', ['init', 'haml', 'png', 'sass', 'css', 'compress', 'vendorCSS'], () => {
+  console.log('Building completed!');
+});
